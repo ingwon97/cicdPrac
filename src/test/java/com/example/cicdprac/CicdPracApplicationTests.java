62de63fbc5f94190ab44b97ad227b008f3cb663f
ingwon97 <ghkdrnjs180@naver.com>
2022-09-13 02:27:57 +0900

    maven -> gradle

--- src/test/java/com/example/cicdprac/CicdPracApplicationTests.java
+++ src/test/java/com/example/cicdprac/CicdPracApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.cicdtest;
+package com.example.cicdprac;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CicdTestApplicationTests {
+class CicdPracApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
