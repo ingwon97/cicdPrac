fed59397d5190b6bce9f805f2d86a80acf916a48
ingwon97 <ghkdrnjs180@naver.com>
2022-09-13 02:10:33 +0900

    세팅

---
+++ src/main/java/com/example/cicdtest/TestController.java
@@ -0,0 +1,14 @@
+package com.example.cicdtest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String test(){
+        String testStr = "CI/CD TEST";
+        System.out.println(testStr);
+        return testStr;
+    }
+}
